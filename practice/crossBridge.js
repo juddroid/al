525162ee4161f60bcd603df9825a0dacb208ea2b
Raccoon <nobllizdc@gmail.com>
2020-11-30 00:47:40 +0900

    프로그래머스 트럭 다리건너기

--- practice/crossBridge.js
+++ practice/crossBridge.js
@@ -1,63 +1,67 @@
-const bridge_length = 100;
-const weight = 100;
-const truck_weights = [10];
+// const bridge_length = 100;
+// const weight = 100;
+// const truck_weights = [10];
 
-// const bridge_length = 2;
-// const weight = 10;
-// const truck_weights = [7, 4, 5, 6];
+function testCase1() {
+  // when
+  const bridge_length = 2;
+  const weight = 10;
+  const truck_weights = [7, 4, 5, 6];
 
-// second
+  // then
+  const result = solution(bridge_length, weight, truck_weights);
+  const expected = 8;
 
-// 다리 길이만큼의 트럭들을 모아서 봤을 때,
-// 무게가 넘지 않으면 시간을 카운트,
-// 무게가 넘으면 공백을 넣어줄까
-
-let bridge = [];
-let goneTruck = [];
-let count = 0;
-const finishNum = truck_weights.length;
-
-for (let i = 0; i < bridge_length; i++) {
-  bridge.push(0);
+  return result == expected;
 }
 
-function currentWeight(arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
+console.log(testCase1());
+
+function solution(bridge_length, weight, truck_weights) {
+  const SPACE = 0;
+  let second = 0;
+  let bridge = [];
+
+  function startWorld() {
+    bridge = new Array(bridge_length).fill(SPACE);
+
+    while (truck_weights.length > 0 || getCurrentBridgeWeight() > 0) {
+      let truck = truck_weights[0];
+      bridge.shift();
+      controlMove(truck);
+      second++;
+    }
+    return second;
   }
-  return sum;
-}
 
-function pushBridge(truck_weights) {
-  bridge.splice(1, 1, truck_weights[0]);
-
-  if (currentWeight(bridge) > weight) {
-    bridge.splice(1, 1, 0);
-  } else if (bridge[1] === undefined) {
-    bridge.splice(1, 1, -1);
-  } else {
-    bridge.splice(1, 1, truck_weights[0]);
-    truck_weights.splice(truck_weights.indexOf(truck_weights[0]), 1);
+  function controlMove(truck) {
+    if (canEnterTruck() && isOverWeightBridge(truck)) {
+      enterTruck(truck);
+    } else {
+      enterEmptySpace(SPACE);
+    }
   }
-  // if (currentWeight(bridge) === -2) {
-  //   console.log('done');
-  // }
-  console.log(bridge);
-  count++;
-}
 
-function goTruck(bridge) {
-  if (bridge[0] === 0) {
-  } else {
-    goneTruck.push(bridge[0]);
+  function enterTruck(truck) {
+    bridge.push(truck);
+    truck_weights.shift();
   }
-  bridge.splice(0, 1, bridge[1]);
-  pushBridge(truck_weights);
-}
 
-while (finishNum !== goneTruck.length) {
-  goTruck(bridge);
-}
+  function enterEmptySpace(space) {
+    bridge.push(space);
+  }
 
-console.log(count);
+  function getCurrentBridgeWeight() {
+    return bridge.reduce((x, y) => x + y, 0);
+  }
+
+  function canEnterTruck() {
+    return bridge_length >= bridge.length - 1;
+  }
+
+  function isOverWeightBridge(truckWeight) {
+    return getCurrentBridgeWeight() + truckWeight <= weight;
+  }
+
+  return startWorld();
+}
