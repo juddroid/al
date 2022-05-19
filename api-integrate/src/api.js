39e26dacfe4c42618e5dc866a17d1169e55ce0ad
Raccoon <nobllizdc@gmail.com>
2022-05-19 23:56:25 +0900

    test webhook2

--- api-integrate/src/api.js
+++ api-integrate/src/api.js
@@ -6,7 +6,7 @@ export async function getUsers() {
 }
 
 export async function getUser(id) {
-  console.log('test');
+  console.log('test2');
   const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
   return response.data;
 }
